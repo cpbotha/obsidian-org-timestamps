0bd291a70e24e58b747c3ee26b06582fafb0ca66
Charl P. Botha <cpbotha@vxlabs.com>
2025-06-02 13:45:54 +0200

    Add 5 minutes forward-backword command

--- main.ts
+++ main.ts
@@ -24,6 +24,65 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: "default",
 };
 
+function forwardTimeByMinutes(curTime: dayjs.Dayjs, minutes = 5): dayjs.Dayjs {
+	// add 5 minutes, then round down to the nearest 5 minutes
+	// (e.g. 09:25 becomes 09:30, 09:28 becomes 09:30, 09:32 becomes 09:35)
+	const newMinute = 5 * Math.floor((curTime.minute() + minutes) / 5.0);
+	const newTime = curTime.minute(newMinute); // this will also take care of going to the next hour if needed
+	return newTime;
+}
+
+function backwardTimeByMinutes(curTime: dayjs.Dayjs, minutes = 5): dayjs.Dayjs {
+	// add 5 minutes, then round down to the nearest 5 minutes
+	// (e.g. 09:25 becomes 09:30, 09:28 becomes 09:30, 09:32 becomes 09:35)
+	const newMinute = 5 * Math.ceil((curTime.minute() - minutes) / 5.0);
+	const newTime = curTime.minute(newMinute); // this will also take care of going to the next hour if needed
+	return newTime;
+}
+
+function modifyTimeUnderCursor(
+	editor: Editor,
+	modTimeFunc: (time: dayjs.Dayjs) => dayjs.Dayjs
+) {
+	// regex should capture timestamp like " 09:25 ", "-09:25 " or "-09:25>"
+	// negative lookbehind (?<!:) ensures there is not a : immediately before
+	// negative lookahead (?!:) ensures there is not a : immediately after.
+	const regex = /(?<!:)(\d{2}:\d{2})(?!:)/g;
+	const cursor = editor.getCursor();
+	// match all timestamps on current line, find one with my cursor on it
+	const line = editor.getLine(cursor.line);
+	const matches = line.matchAll(regex);
+	//console.log(...matches);
+	for (const match of matches) {
+		console.log(match);
+		if (
+			match.index &&
+			cursor.ch >= match.index &&
+			cursor.ch <= match.index + match[0].length
+		) {
+			// match[0] is the full match, e.g. " 09:25 "
+			// match[1] is the capture group timestamp, e.g. "09:25"
+			const curTime = dayjs(match[1], "HH:mm");
+
+			const newTime = modTimeFunc(curTime);
+			const newTimeStr = newTime.format("HH:mm");
+			const newCh = match.index + match[0].indexOf(match[1]);
+			editor.replaceRange(
+				newTimeStr,
+				{
+					line: cursor.line,
+					ch: newCh,
+				},
+				{ line: cursor.line, ch: newCh + newTimeStr.length }
+			);
+			// put cursor back where user started
+			// (when you overwrite, cursor is reset to start of overwrite)
+			editor.setCursor(cursor);
+			break;
+		}
+	}
+}
+
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
@@ -52,42 +111,18 @@ export default class MyPlugin extends Plugin {
 			id: "timestamp-forward-5min",
 			name: "Forward timestamp by 5 minutes",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				// regex should capture timestamp like " 09:25 ", "-09:25 " or "-09:25>"
-				const regex = /[^:](\d{2}:\d{2})[^:]/g;
-				const cursor = editor.getCursor();
-				// match all timestamps on current line, find one with my cursor on it
-				const line = editor.getLine(cursor.line);
-				const matches = line.matchAll(regex);
-				for (const match of matches) {
-					if (
-						match.index &&
-						cursor.ch >= match.index &&
-						cursor.ch <= match.index + match[0].length
-					) {
-						// match[0] is the full match, e.g. " 09:25 "
-						// match[1] is the capture group timestamp, e.g. "09:25"
-						const curTime = dayjs(match[1], "HH:mm");
-						// add 5 minutes, then round down to the nearest 5 minutes
-						// (e.g. 09:25 becomes 09:30, 09:28 becomes 09:30, 09:32 becomes 09:35)
-						const newMinute =
-							5 * Math.floor((curTime.minute() + 5) / 5.0);
-						const newTime = curTime.minute(newMinute); // this will also take care of going to the next hour if needed
-						const newTimeStr = newTime.format("HH:mm");
-						const newCh = match.index + match[0].indexOf(match[1]);
-						editor.replaceRange(
-							newTimeStr,
-							{
-								line: cursor.line,
-								ch: newCh,
-							},
-							{ line: cursor.line, ch: newCh + newTimeStr.length }
-						);
-						// put cursor back where user started
-						// (when you overwrite, cursor is reset to start of overwrite)
-						editor.setCursor(cursor);
-						break;
-					}
-				}
+				modifyTimeUnderCursor(editor, (curTime) => {
+					return forwardTimeByMinutes(curTime, 5);
+				});
+			},
+		});
+		this.addCommand({
+			id: "timestamp-back-5min",
+			name: "Backward timestamp by 5 minutes",
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				modifyTimeUnderCursor(editor, (curTime) => {
+					return backwardTimeByMinutes(curTime, 5);
+				});
 			},
 		});
 		// This adds a settings tab so the user can configure various aspects of the plugin
